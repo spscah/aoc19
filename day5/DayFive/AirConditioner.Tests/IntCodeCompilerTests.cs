e22707156d5636ef830f645807e6c772b7685600
Chris Harrison <cah@stpaulsschool.org.uk>
2019-12-11 14:54:59 +0000

    Day Five

---
+++ day5/DayFive/AirConditioner.Tests/IntCodeCompilerTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayFive;
+
+namespace AirConditioner.Tests
+{
+    [TestClass]
+    public class IntCodeCompilerTests
+    {
+        
+        [TestMethod]
+        [DataRow(8, 1)]
+        [DataRow(9, 0)]
+        [DataRow(7, 0)]
+        public void TestGiven1(int i, int e)
+        {
+            IList<int> input = new List<int> { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+        [TestMethod]
+        [DataRow(8, 0)]
+        [DataRow(9, 0)]
+        [DataRow(7, 1)]
+        public void TestGiven2(int i, int e)
+        {
+            IList<int> input = new List<int> { 3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+
+        [TestMethod]
+        [DataRow(8, 1)]
+        [DataRow(9, 0)]
+        [DataRow(7, 0)]
+        public void TestGiven3(int i, int e)
+        {
+            IList<int> input = new List<int> { 3, 3, 1108, -1, 8, 3, 4, 3, 99 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+
+        [TestMethod]
+        [DataRow(8, 0)]
+        [DataRow(9, 0)]
+        [DataRow(7, 1)]
+        public void TestGiven4(int i, int e)
+        {
+            IList<int> input = new List<int> { 3, 3, 1107, -1, 8, 3, 4, 3, 99 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 1)]
+        public void TestGiven5a(int i, int e)
+        {
+            IList<int> input = new List<int> { 3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+        [TestMethod]
+        [DataRow(0, 0)]
+        [DataRow(1, 1)]
+        public void TestGiven5b(int i, int e)
+        {
+            IList<int> input = new List<int> { 3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+        [TestMethod]
+        [DataRow(7, 999)]
+        [DataRow(8, 1000)]
+        [DataRow(9, 1001)]
+        public void TestGiven6(int i, int e)
+        {
+            IList<int> input = new List<int> { 3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99 };
+            var inputs = new Stack<int>();
+            inputs.Push(i);
+            var icc = new IntCodeCompiler(inputs);
+            icc.Calculate(input);
+            Assert.AreEqual(e, icc.LastOutput);
+        }
+
+
+
+    }
+}
---
+++ day5/DayFive/AirConditioner.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AirConditioner.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("AirConditioner.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9094719f-1d51-44cf-a245-8824e46611d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
