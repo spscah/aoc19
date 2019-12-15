8e581f186afc49f23b73fe590aa48a76bf69b76b
Chris Harrison <cah@stpaulsschool.org.uk>
2019-12-15 16:14:36 +0000

    Day Ten, part 2

--- day10/DayTen/DayTen.Lib/MonitoringStation.cs
+++ day10/DayTen/DayTen.Lib/MonitoringStation.cs
@@ -17,29 +17,55 @@ namespace DayTen.Lib
             _points = Enumerable.Range(0, map.Length).Where(i => map[i] == '#').Select(i => new Point(i, _sideLength)).ToList();
         }
 
-        public int MaxVisible
+
+        public int VaporiseOrder(int i)
         {
-            get
+            // this is massively inefficient 
+            IList<int> visibility = _points.Select(p => Visible(p).Count).ToList();
+            int m = visibility.Max();
+            int ind = visibility.IndexOf(m);
+            Point centre = _points[ind];
+            if( i > m)
             {
-                // var results = Enumerable.Range(0, _map.Length).Select(i => Visible(i)).ToList();
-                var results = _points.Select(i => Visible(i)).ToList();
-                var m = results.Max();
-                var ind = results.IndexOf(m);
-                return results.Max();
+                // hack - huge fudge 
+                throw new NotImplementedException();
             }
+            IList<Point> innershell = Visible(centre);
+            IDictionary<Point, double> angles = innershell.ToDictionary(p => p, p => centre.AngleToPoint(p));
+            int counter = 0;
+            foreach (var pair in angles.OrderBy(p => p.Value))
+            {
+                // work with pair.Key and pair.Value
+                ++counter;
+                if (counter == i)
+                    return (100 * pair.Key.X) + pair.Key.Y;
+            }
+            return -1;
         }
 
-        int Visible(Point p)
+
+        public int MaxVisible(bool returnIndex=false)
         {
-            IList<Point> others = _points.Where(i => !i.Equals(p)).ToList();
-            foreach(Point blocker in _points.Where(i => !i.Equals(p)))
+            // var results = Enumerable.Range(0, _map.Length).Select(i => Visible(i)).ToList();
+            var results = _points.Select(i => Visible(i).Count).ToList();
+            var m = results.Max();
+            var ind = results.IndexOf(m);
+            return returnIndex ? ind : results.Max();
+        }
+
+        IList<Point> Visible(Point p, IList<Point> pointsToUse = null)
+        {
+            if (pointsToUse == null)
+                pointsToUse = _points;
+            IList<Point> others = pointsToUse.Where(i => !i.Equals(p)).ToList();
+            foreach(Point blocker in pointsToUse.Where(i => !i.Equals(p)))
             {
                 var potentiallyblocked = others.Where(i => !i.Equals(blocker)).Where(i => blocker.OnTheLineBetween(p, i)).Select(i => i).ToList();
                 foreach (Point doomed in potentiallyblocked)
                     others.Remove(doomed);
                  
             }
-            return others.Count;
+            return others;
         }
     }
 
@@ -48,6 +74,10 @@ namespace DayTen.Lib
         public int X;
         public int Y;
 
+        public Point(int x, int y, bool mcguffin)
+        {
+            X = x; Y = y;
+        }
         public Point(int i, int sl)
         {
             X = i % sl;
@@ -56,7 +86,40 @@ namespace DayTen.Lib
 
         public bool Equals(Point other)
         {
+            if (other == null)
+                return false;
             return X == other.X && Y == other.Y;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj as Point == null ? false : Equals((Point)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            // https://stackoverflow.com/questions/263400/what-is-the-best-algorithm-for-overriding-gethashcode
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 31;
+                // Suitable nullity checks etc, of course :)
+                hash = hash * 37 + X.GetHashCode();
+                hash = hash * 37 + Y.GetHashCode();
+                return hash;
+            }
+        }
+
+        public Point ReturnNearestAndRemove(IList<Point> list)
+        {
+            var closest = list.Select(p => Distance(p)).Min();
+            var point = list.Where(p => Math.Abs(Distance(p) - closest) < double.Epsilon).First();
+            list.Remove(point);
+            return point;
+        }
+
+        double Distance(Point p)
+        {
+            double d= Math.Sqrt(((p.X - X) * (p.X - X)) + ((p.Y - Y) * (p.Y - Y)));
+            return d;
         }
 
         bool CouldBe(Point oneCorner, Point otherCorner)
@@ -110,6 +173,29 @@ namespace DayTen.Lib
         public override string ToString()
         {
             return string.Format("({0},{1})", X, Y);
+        }
+
+        internal double AngleToPoint(Point other)
+        {
+            if (Math.Abs(other.X - X) < double.Epsilon)
+                return (other.Y > Y) ? 180 : 0;
+            if (Math.Abs(other.Y - Y) < double.Epsilon)
+                return (other.X > X) ? 90 : 270;
+
+            double angle = 360 * Math.Atan(Math.Abs(other.X - X) / (double)Math.Abs(other.Y - Y)) / (2d * Math.PI);
+            if(other.X > X)
+            {
+                if (other.Y > Y)
+                    return 180 - angle;
+                else
+                    return 0 + angle;
+            } else
+            {
+                if (other.Y < Y)
+                    return 360 - angle;
+                else
+                    return 180 + angle;
+            }
         }
     }
 }
--- day10/DayTen/DayTen.Tests/MonitoringStationTests.cs
+++ day10/DayTen/DayTen.Tests/MonitoringStationTests.cs
@@ -11,68 +11,103 @@ namespace DayTen.Tests
         public void TestGiven1()
         {
             var ms = new MonitoringStation(".#..#.....#####....#...##");
-            Assert.AreEqual(8, ms.MaxVisible);
+            Assert.AreEqual(8, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestGiven2()
         {
             var ms = new MonitoringStation("......#.#.#..#.#......#######..#.#.###...#..#.......#....#.##..#....#..##.#..#####...#..#..#....####");
-            Assert.AreEqual(33, ms.MaxVisible);
+            Assert.AreEqual(33, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestGiven3()
         {
             var ms = new MonitoringStation("#.#...#.#..###....#..#....#...##.#.#.#.#....#.#.#..##..###.#..#...##....##....##......#....####.###.");
-            Assert.AreEqual(35, ms.MaxVisible);
+            Assert.AreEqual(35, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestGiven4()
         {
             var ms = new MonitoringStation(".#..#..#######.###.#....###.#...###.##.###.##.#.#.....###..#..#.#..#.##..#.#.###.##...##.#.....#.#..");
-            Assert.AreEqual(41, ms.MaxVisible);
+            Assert.AreEqual(41, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestGiven5()
         {
             var ms = new MonitoringStation(".#..##.###...#########.############..##..#.######.########.#.###.#######.####.#.#####.##.#.##.###.##..#####..#.##############################.####....###.#.#.####.######################.##.###..####....######..##.###########.##.####...##..#.#####..#.######.#####...#.##########...#.##########.#######.####.#.###.###.#.##....##.##.###..#####.#.#.###########.####.#.#.#####.####.######.##.####.##.#..##");
-            Assert.AreEqual(210, ms.MaxVisible);
+            Assert.AreEqual(210, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestStraightLine()
         {
             var ms = new MonitoringStation("#....#....#....#");
-            Assert.AreEqual(2, ms.MaxVisible);
+            Assert.AreEqual(2, ms.MaxVisible());
         }
         [TestMethod]
         public void TestNegativeStraightLine()
         {
             var ms = new MonitoringStation("...#..#..#..#...");
-            Assert.AreEqual(2, ms.MaxVisible);
+            Assert.AreEqual(2, ms.MaxVisible());
         }
         [TestMethod]
         public void TestVerticalLine()
         { 
             var ms = new MonitoringStation("#....#....#....#....#....");
-            Assert.AreEqual(2, ms.MaxVisible);
+            Assert.AreEqual(2, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestHorizontalLine()
         {
             var ms = new MonitoringStation("####............");
-            Assert.AreEqual(2, ms.MaxVisible);
+            Assert.AreEqual(2, ms.MaxVisible());
         }
 
         [TestMethod]
         public void TestStraightLine2()
         {
             var ms = new MonitoringStation("####...........#");
-            Assert.AreEqual(4, ms.MaxVisible);
+            Assert.AreEqual(4, ms.MaxVisible());
         }
+
+        [TestMethod]
+        public void TestPart2a()
+        {
+            var ms = new MonitoringStation(".#..##.###...#########.############..##..#.######.########.#.###.#######.####.#.#####.##.#.##.###.##..#####..#.##############################.####....###.#.#.####.######################.##.###..####....######..##.###########.##.####...##..#.#####..#.######.#####...#.##########...#.##########.#######.####.#.###.###.#.##....##.##.###..#####.#.#.###########.####.#.#.#####.####.######.##.####.##.#..##");
+            Assert.AreEqual(1112, ms.VaporiseOrder(1));
+            Assert.AreEqual(1201, ms.VaporiseOrder(2));
+            Assert.AreEqual(1202, ms.VaporiseOrder(3));
+            Assert.AreEqual(1208, ms.VaporiseOrder(10));
+            Assert.AreEqual(1600, ms.VaporiseOrder(20));
+            Assert.AreEqual(1609, ms.VaporiseOrder(50));
+            Assert.AreEqual(1016, ms.VaporiseOrder(100));
+        }
+        [TestMethod]
+        public void TestPart2b()
+        {
+            var ms = new MonitoringStation(".#..##.###...#########.############..##..#.######.########.#.###.#######.####.#.#####.##.#.##.###.##..#####..#.##############################.####....###.#.#.####.######################.##.###..####....######..##.###########.##.####...##..#.#####..#.######.#####...#.##########...#.##########.#######.####.#.###.###.#.##....##.##.###..#####.#.#.###########.####.#.#.#####.####.######.##.####.##.#..##");
+            Assert.AreEqual(906, ms.VaporiseOrder(199));
+            //            Assert.AreEqual(1101, ms.VaporiseOrder(299));
+        }
+        [TestMethod]
+        public void TestPart2c()
+        {
+            var ms = new MonitoringStation(".#..##.###...#########.############..##..#.######.########.#.###.#######.####.#.#####.##.#.##.###.##..#####..#.##############################.####....###.#.#.####.######################.##.###..####....######..##.###########.##.####...##..#.#####..#.######.#####...#.##########...#.##########.#######.####.#.###.###.#.##....##.##.###..#####.#.#.###########.####.#.#.#####.####.######.##.####.##.#..##");
+            Assert.AreEqual(802, ms.VaporiseOrder(200));
+            //            Assert.AreEqual(1101, ms.VaporiseOrder(299));
+        }
+        [TestMethod]
+        public void TestPart2d()
+        {
+            var ms = new MonitoringStation(".#..##.###...#########.############..##..#.######.########.#.###.#######.####.#.#####.##.#.##.###.##..#####..#.##############################.####....###.#.#.####.######################.##.###..####....######..##.###########.##.####...##..#.#####..#.######.#####...#.##########...#.##########.#######.####.#.###.###.#.##....##.##.###..#####.#.#.###########.####.#.#.#####.####.######.##.####.##.#..##");
+            Assert.AreEqual(1009, ms.VaporiseOrder(201));
+            //            Assert.AreEqual(1101, ms.VaporiseOrder(299));
+        }
+
     }
 }
